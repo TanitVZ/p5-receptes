30be63bd89c17a0b5bd365d06897dc896fdf6178
Laia Valenti <laia.valenti@gmail.com>
2024-06-17 20:13:40 +0200

    Creo camps ingredients

--- components/RecipeData.tsx
+++ components/RecipeData.tsx
@@ -1,8 +1,31 @@
+"use client"
+
 import React from 'react'
 import type { Recipe } from '@/lib/receptes'
+import { Input } from '@/components/ui/input'
+
+import { Select,  SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 export default function RecipeData({recipes} : {recipes : Recipe}) {
   return (
+    <>
     <h1>{recipes.titol}</h1>
+    <div>
+      <label>Quantitat</label>
+      <Input type="number" placeholder="quantitat"/>
+      <label>Mesura</label>
+      <Select>
+        <SelectTrigger>
+          <SelectValue placeholder="Sel·lecciona mesura"/>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="Kg">Kg</SelectItem>
+          <SelectItem value="l">l</SelectItem>
+        </SelectContent>
+      </Select>
+      <label>Ingredient</label>
+      <Input type="text" placeholder="ingredient"/>
+    </div>
+</>
   )
 }
