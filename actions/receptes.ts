0dc4c7e6fa00a3c0bd40d3d90b01fc517a660f44
Laia Valenti <laia.valenti@gmail.com>
2024-06-23 12:10:05 +0200

    Eliminar ingredients amb n receptes

--- actions/receptes.ts
+++ actions/receptes.ts
@@ -1,7 +1,7 @@
 "use server";
 import RecipeData from "@/components/RecipeData";
 import { IngredientType, RecipeType, RecipesType } from "@/lib/receptes";
-import  { readFile, writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { revalidatePath } from "next/cache";
 import path from "path";
 
@@ -11,14 +11,12 @@ export async function actionReadRecepta(id: number) {
   const jsonData = await readFile(jsonFilePath, "utf8");
 
   const recipes = JSON.parse(jsonData);
-  
-  const recipe = recipes.receptes.find((recep: RecipeType) => recep.id === id);
-  
-  if (recipe) {
 
+  const recipe = recipes.receptes.find((recep: RecipeType) => recep.id === id);
+
+  if (recipe) {
     return recipe;
-  }
-  else console.log("no hi ha recepta");
+  } else console.log("no hi ha recepta");
 }
 
 export async function actionReadReceptes() {
@@ -26,29 +24,36 @@ export async function actionReadReceptes() {
   const recipes = JSON.parse(jsonData);
   return recipes;
 }
- 
-export async function actionUpdateReceptes(updatedRecipes: RecipeType) {
 
-  //NO CAL TORNAR A LLEGIR TOT EL JSON, JA EL TENIM 
+export async function actionUpdateReceptes(updatedRecipes: RecipeType) {
+  //NO CAL TORNAR A LLEGIR TOT EL JSON, JA EL TENIM
   // No estava bé el Home
   /*
   const jsonData = fs.readFileSync(jsonFilePath, "utf8");
   console.log(JSON.stringify(updatedRecipes));
 */
 
-//Amb dues receptes el torno a llegir --> no crec que sigui l'opció òptima
-const jsonData = await readFile(jsonFilePath, "utf8");
-const recipes : RecipesType = JSON.parse(jsonData);
-const recipeIndex = updatedRecipes.id - 1
+  //Amb dues receptes el torno a llegir --> no crec que sigui l'opció òptima
+  
+  //TODO --> s'hauria de passar un objecte del tipus RecipesType enlloc de tornar a llegir
+  //TODO --> mirar perquè al afegir ingredient, fa dos POST
 
-recipes.receptes[recipeIndex] = updatedRecipes
+  const jsonData = await readFile(jsonFilePath, "utf8");
+  const recipes: RecipesType = JSON.parse(jsonData);
+  const recipeIndex = updatedRecipes.id - 1;
 
-const updatedJson = JSON.stringify(recipes)
-await writeFile(jsonFilePath, updatedJson);
+  recipes.receptes[recipeIndex] = updatedRecipes;
+
+  const updatedJson = JSON.stringify(recipes);
+  await writeFile(jsonFilePath, updatedJson);
   revalidatePath("/");
 }
 
 export async function actionDeleteIngredient(id: Number, recipes: RecipeType) {
+  const jsonData = await readFile(jsonFilePath, "utf8");
+  const recipesAll: RecipesType = JSON.parse(jsonData);
+  const recipeIndex = recipes.id - 1;
+
   const updatedJson = {
     ...recipes,
     ingredients: recipes.ingredients.filter(
@@ -56,6 +61,8 @@ export async function actionDeleteIngredient(id: Number, recipes: RecipeType) {
     ),
   };
 
-  await writeFile (jsonFilePath, JSON.stringify(updatedJson));
+  recipesAll.receptes[recipeIndex] = updatedJson;
+
+  await writeFile(jsonFilePath, JSON.stringify(recipesAll));
   revalidatePath("/");
 }
